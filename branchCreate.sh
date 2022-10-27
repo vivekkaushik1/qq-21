5a31dccec0221e54e635900d59904921f107bd7e
vivekkaushik1 <vivekit388@gmail.com>
2022-10-27 19:32:24 +0530

    2:randfile_2

--- branchCreate.sh
+++ branchCreate.sh
@@ -10,6 +10,7 @@ do
   rm -rf random*
   git add .
   git commit -m "ADO Commits"
+  sleep 10.0
   #git tag -a v1.1 -m "version 1.1"
   #git tag -a v2.1 -m "version 2.1"
   #git push --tags origin
--- createCOmmit.sh
+++ createCOmmit.sh
@@ -3,11 +3,16 @@ x=1
 j=1
 while [ $x -le 5 ]
  do
-   git add .
-   git commit -m "ADO Commits"
+   string="touch randomfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
+   echo $string
+   eval "$string"
+   x=$[$x+1]
+   #sleep 7.0
+   #git add .
+   #git commit -m "ADO Commits"
    #git tag -a PerformanceTest11.$((j+x)) -m "vversion Pt1.1$((j+x))"
    #sh ./createCOmmitsTags.sh
-   x=$(( $x + 1 ))
+   #x=$(( $x + 1 ))
    echo "Completed Script Invocaton"
 done
 git push
