6fc06a2ce5be3e62e966fab21ffc722225082fe5
vivekkaushik1 <vivekit388@gmail.com>
2022-10-27 19:25:41 +0530

    ADO Commits

--- createCOmmit.sh
+++ createCOmmit.sh
@@ -3,7 +3,9 @@ x=1
 j=1
 while [ $x -le 5 ]
  do
-   git tag -a PerformanceTest11.$((j+x)) -m "vversion Pt1.1$((j+x))"
+   git add .
+   git commit -m "ADO Commits"
+   #git tag -a PerformanceTest11.$((j+x)) -m "vversion Pt1.1$((j+x))"
    #sh ./createCOmmitsTags.sh
    x=$(( $x + 1 ))
    echo "Completed Script Invocaton"
