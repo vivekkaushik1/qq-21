03c10c99ff866c89c69693afcdefafee4ddba749
vivekkaushik1 <vivekit388@gmail.com>
2022-11-25 15:30:03 +0530

    2:randfile_2

--- createCOmmit.sh
+++ createCOmmit.sh
@@ -3,12 +3,13 @@ x=1
 j=1
 while [ $x -le 1000 ]
  do
+   rm -rf randomfile*  
    string="touch randomfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
    echo $string
    eval "$string"
    x=$[$x+1]
    sleep 10.0
-   #git add .
+   git add .
    git commit -m "ADO Commits"
    #git tag -a PerformanceTest11.$((j+x)) -m "vversion Pt1.1$((j+x))"
    #sh ./createCOmmitsTags.sh
